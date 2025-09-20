bcc0c0f8ee03308b9953ee40dda241df44e5bf87
Alyssa Nguyen <18040427+Alyssa3467@users.noreply.github.com>
2025-09-20 00:29:15 -0700

    bug fixes

--- customtoolchain.sh
+++ customtoolchain.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set up environment, maybe
 if [ -f ".env" ]; then
@@ -33,30 +33,34 @@ fi
 mkdir -p "${PROJECTROOT}/raspberrypi"
 mkdir -p "${SOURCEROOT}"
 
-cd ${SOURCEROOT}
+cd "${SOURCEROOT}"
 [ -f binutils-*.tar.bz2 ] || wget https://ftpmirror.gnu.org/binutils/binutils-2.45.tar.bz2
 [ -f gcc-*.tar.gz ]       || wget https://ftpmirror.gnu.org/gcc/gcc-15.2.0/gcc-15.2.0.tar.gz
 [ -f mpfr-*.tar.bz2 ]     || wget https://ftpmirror.gnu.org/mpfr/mpfr-4.2.2.tar.bz2
 [ -f gmp-*.tar.bz2 ]      || wget https://ftpmirror.gnu.org/gmp/gmp-6.3.0.tar.bz2
 [ -f mpc-*.tar.gz  ]      || wget https://ftpmirror.gnu.org/mpc/mpc-1.3.1.tar.gz
-[ -f glibc-*.tar.gz ]     || wget https://ftpmirror.gnu.org/gnu/glibc/glibc-2.42.tar.bz2
+[ -f glibc-*.tar.bz2 ]    || wget https://ftpmirror.gnu.org/gnu/glibc/glibc-2.42.tar.bz2
 
-cd ${PROJECTROOT}/raspberrypi
-[ -d ./linux ] || git clone git@github.com:raspberrypi/linux ${PROJECTROOT}/raspberrypi/linux
+cd "${PROJECTROOT}/raspberrypi"
+[ -d ./linux ] || git submodule add git@github.com:raspberrypi/linux "${PROJECTROOT}/raspberrypi/linux"
 
-cd ${PROJECTROOT}/raspberrypi/linux
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} bcmrpi_defconfig
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} zImage modules dtbs | tee ${PROJECTROOT}/build.log
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} INSTALL_HDR_PATH=${SYSROOT} headers_install
+cd "${PROJECTROOT}/raspberrypi/linux"
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} bcmrpi_defconfig V=2
+# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} zImage modules dtbs V=2 | tee ${PROJECTROOT}/build.log
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} INSTALL_HDR_PATH=${SYSROOT} headers_install V=2
 
-mkdir -p ${BUILDROOT}/build-binutils && cd ${BUILDROOT}/build-binutils
-tar -xvf ${SOURCEROOT}/binutils-2.45.tar.bz2
-cd binutils-2.45
-./configure --target=${CCPREFIX} --prefix=${XBIN_FOLDER} --with-sysroot=${SYSROOT} --disable-multilib
+cd "${SOURCEROOT}"
+tar -xvf "${SOURCEROOT}/binutils-2.45.tar.bz2"
+mkdir -p "${BUILDROOT}/build-binutils" && cd "${BUILDROOT}/build-binutils"
+"${SOURCEROOT}/binutils-2.45/configure" \
+    --target=${CCPREFIX} \
+    --prefix=${XBIN_FOLDER} \
+    --with-sysroot=${SYSROOT} \
+    --disable-multilib
 make -j$(nproc)
 make install
 
-cd ${SOURCEROOT}
+cd "${SOURCEROOT}"
 tar -xvf mpfr-4.2.2.tar.bz2
 tar -xvf gmp-6.3.0.tar.bz2
 tar -xvf mpc-1.3.1.tar.gz
@@ -65,41 +69,46 @@ cd gcc-15.2.0
 ln -s ../mpfr-4.2.2 mpfr
 ln -s ../gmp-6.3.0  gmp
 ln -s ../mpc-1.3.1  mpc
-mkdir -p ${BUILDROOT}/build-gcc && cd ${BUILDROOT}/build-gcc
-${SOURCEROOT}/gcc-15.2.0/configure --target=${CCPREFIX} --prefix=${XBIN_FOLDER} --with-sysroot=${SYSROOT} \
-    --enable-languages=c --disable-multilib --without-headers --disable-bootstrap
-make all-gcc -j$(nproc)
-make install-gcc
+mkdir -p "${BUILDROOT}/build-gcc" && cd "${BUILDROOT}/build-gcc"
+"${SOURCEROOT}/gcc-15.2.0/configure" \
+    --target=${CCPREFIX} \
+    --prefix=${XBIN_FOLDER} \
+    --with-sysroot=${SYSROOT} \
+    --disable-multilib \
+    --enable-languages=c \
+    --without-headers \
+    --disable-bootstrap
+make all-gcc -j$(nproc) V=sc
+make install-gcc V=sc
 
-cd ${SOURCEROOT}
-tar -xvf ${SOURCEROOT}/glibc-2.42.tar.bz2
-mkdir -p ${BUILDROOT}/build-glibc && cd ${BUILDROOT}/build-glibc
-${SOURCEROOT}/glibc-2.42/configure \
-    --build=${uname -m}-linux-gnu \
+cd "${SOURCEROOT}"
+tar -xvf "${SOURCEROOT}/glibc-2.42.tar.bz2"
+mkdir -p "${BUILDROOT}/build-glibc" && cd "${BUILDROOT}/build-glibc"
+"${SOURCEROOT}/glibc-2.42/configure" \
+    --build="$(uname -m)-linux-gnu" \
     --host=${CCPREFIX} \
     --with-sysroot=${SYSROOT} \
-    --with-headers=${SYSROOT}/usr/include \
+    --with-headers="${SYSROOT}/include" \
     --prefix=/usr \
     --enable-kernel=3.2 \
     --disable-multilib
 make install-bootstrap-headers=yes install-headers cross-compiling=yes
 make -j$(nproc) csu/subdir_lib
-install csu/crt1.o csu/crti.o csu/crtn.o ${SYSROOT}/usr/lib
-${CCPREFIX}gcc -nostdlib -nostartfiles -shared -x c /dev/null -o ${SYSROOT}/usr/lib/libc.so
-touch ${SYSROOT}/usr/include/gnu/stubs.h
+install csu/crt1.o csu/crti.o csu/crtn.o "${SYSROOT}/usr/lib"
+"${CCPREFIX}gcc" -nostdlib -nostartfiles -shared -x c /dev/null -o "${SYSROOT}/usr/lib/libc.so"
+touch ${SYSROOT}/include/gnu/stubs.h
 
-mkdir -p ${BUILDROOT}/build-gcc-stage2 && cd ${BUILDROOT}/build-gcc-stage2
-${SOURCEROOT}/gcc-15.2.0/configure \
+mkdir -p "${BUILDROOT}/build-gcc-stage2" && cd "${BUILDROOT}/build-gcc-stage2"
+"${SOURCEROOT}/gcc-15.2.0/configure" \
     --target=${CCPREFIX} \
     --prefix=${XBIN_FOLDER} \
     --with-sysroot=${SYSROOT} \
+    --with-headers="${SYSROOT}/include" \
     --enable-languages=c,c++ \
     --disable-multilib \
     --disable-bootstrap \
     --enable-shared \
     --enable-threads=posix \
-    --enable-__cxa_atexit \
-    --with-headers=${SYSROOT}/usr/include
-
+    --enable-__cxa_atexit
 make -j$(nproc)
 make install
